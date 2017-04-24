63ec50e6a2fcd2bed3661ffa6efb5819395aa2b2
Ryan Ralph <ryan@ryanralph.net>
2017-04-24 22:45:38 +1000

    Change data structure to suit Particle Photo WebHooks \ Update max sensor value to 1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 var sensorStates = [0, 0]
 var locationId = 0
-var MAX_SENSOR_RANGE = 10,
+var MAX_SENSOR_RANGE = 1,
 	 MIN_SENSOR_RANGE = 0
 
 
@@ -18,16 +18,10 @@ app.route('/').get(function(req, res) {
 	res.json({"hello": "world"})
 })
 
-app.route('/echo').get(function(req, res) {
-	console.log('GOT /echo')
-	res.json(req.body)
-	console.log(req.body)
-})
-
 app.route('/echo').post(function(req, res) {
 	console.log('POST /echo')
-	res.json(req.body)
-	console.log(JSON.stringify(req.body))
+	res.send(req.body.data)
+	console.log(JSON.stringify(req.body.data))
 })
 
 app.route('/location/:id').get(function(req, res) {
@@ -35,7 +29,7 @@ app.route('/location/:id').get(function(req, res) {
 	console.log('GOT /location/' + locationId)
 	returnVal = {"status": "OK",
 					"locationId": locationId,
-					"SensorState": sensorStates[locationId]}
+					"sensorState": sensorStates[locationId]}
 	res.json(returnVal)
 	console.log(returnVal)
 	returnVal = {}
@@ -43,24 +37,26 @@ app.route('/location/:id').get(function(req, res) {
 
 app.route('/location/:id').post(function(req, res) {
 	console.log('POST /location/' + req.params.id)
-	if(!req.body.locationId) {
+	var data = JSON.parse(req.body.data)
+	if(!data.locationId) {
 		returnVal = {"status": "WRONGPARAMS"}
+		console.log(req.body['data'])
 	} else {
-		if(!req.body.sensorState) {
+		if(!data.sensorState) {
 			returnVal = {"status": "NOSTATE"}
 		} else {
-			locationId = req.body.locationId
+			locationId = data.locationId
 			if(locationId > ((sensorStates.length)-1)) {
 				returnVal = {"status": "ILLEGALLOCATION"}
 			} else {
-				sensorState = req.body.sensorState
+				sensorState = data.sensorState
 				if((sensorState > MAX_SENSOR_RANGE) || (sensorState < MIN_SENSOR_RANGE)) {
 					returnVal = {"status": "SENSOROUTOFRANGE"}
 				} else {
-					sensorStates[locationId] = req.body.sensorState
+					sensorStates[locationId] = sensorState
 					returnVal = {"status": "OK",
 									"locationId": locationId,
-									"SensorState": sensorStates[locationId]}
+									"sensorState": sensorStates[locationId]}
 				}
 			}
 		}
